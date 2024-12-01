25590f4d3ede72047950379028ae917103718159
Micah Wood <micah@wpscholar.com>
2024-12-01 17:02:42 -0500

    Bump version

--- mpress-hide-from-search.php
+++ mpress-hide-from-search.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Hide from Search
  * Plugin URI:        https://wordpress.org/plugins/mpress-hide-from-search/
  * Description:       Hide individual WordPress pages from search engines and/or WordPress search results.
- * Version:           1.1.6
+ * Version:           1.1.7
  * Requires PHP:      7.4
  * Requires at least: 6.0
  * Author:            Micah Wood
@@ -22,7 +22,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-define( 'HIDE_FROM_SEARCH_VERSION', '1.1.6' );
+define( 'HIDE_FROM_SEARCH_VERSION', '1.1.7' );
 define( 'HIDE_FROM_SEARCH_FILE', __FILE__ );
 
 require __DIR__ . '/vendor/autoload.php';
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: search, search engines, WordPress search, hide from search, exclude from s
 Requires at least: 6.0
 Requires PHP: 7.4
 Tested up to: 6.7
-Stable tag: 1.1.6
+Stable tag: 1.1.7
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
